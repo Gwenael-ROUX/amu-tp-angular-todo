d36d7661dad983a59985fd9f76e4544589999cc6
ROUX Gwenaël <37573231+Gwenael-ROUX@users.noreply.github.com>
2022-01-13 10:10:57 +0100

    fin add item

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tasks } from './types/task.type';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +8,32 @@ import { Component } from '@angular/core';
         <h1>La Todo App</h1>
 
         <main>
-          <app-todo-list 
+          <app-todo-list
             [tasks]="tasks"
           ></app-todo-list>
+          <app-task-form
+            (onNewTask)="addTask($event)"
+          ></app-task-form>
         </main>
     </div>
   `,
   styles: []
 })
 export class AppComponent {
-  tasks = [
+  tasks: Tasks = [
     { id: 1, text: "Aller faire des courses", done: false },
     { id: 2, text: "Faire à manger", done: true },
   ];
+
+  // La méthode addTask recevra une string
+  addTask(text: string) {
+    // Elle s'en servira pour créer une nouvelle tâche dans
+    // le tableau des tâches, et Angular mettra à jour
+    // l'affichage afin d'en tenir compte !
+    this.tasks.push({
+      id: Date.now(),
+      text: text,
+      done: false
+    });
+  }
 }
--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form.component';
 import { TodoListComponent } from './todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent, TodoListComponent
+    AppComponent,
+    TodoListComponent,
+    TaskFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // En important le ReactiveFormsModule, on importe des
+    // composants, directives et services qu'il met à notre
+    // disposition !
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
